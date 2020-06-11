65ddd815f5d85dae3e8f5d650aebe9c3afb0d3e4
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-06-10 21:08:03 -0700

    add combine.js file  with data from all sources

--- combinejson.js
+++ combinejson.js
@@ -1,1 +1,26 @@
 const fs = require("fs");
+
+const basicSourceFile = JSON.parse(fs.readFileSync("json-files/basic.json"));
+//console.log(basicSourceFile);
+const intermediateSourceFile = JSON.parse(
+   fs.readFileSync("json-files/intermediate.json")
+);
+//console.log(intermediateSourceFile);
+const functionalSourceFile = JSON.parse(
+   fs.readFileSync("json-files/functional.json")
+);
+//console.log(functionalSourceFile);
+const algorithmSourceFile = JSON.parse(
+   fs.readFileSync("json-files/algorithm.json")
+);
+
+allFiles = basicSourceFile.concat(
+   intermediateSourceFile,
+   functionalSourceFile,
+   algorithmSourceFile
+);
+console.log("comcat", allFiles);
+
+const targetFile = "./dist/dist.json";
+
+fs.writeFileSync(targetFile, JSON.stringify(allFiles)); // js obj not readable stringify JSON
